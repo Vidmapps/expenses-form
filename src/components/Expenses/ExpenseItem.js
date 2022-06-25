536b0704055f98a5f62131a5211f0103a2ef43c1
vidmantas burba <vidmantas153@gmail.com>
2022-06-25 12:40:31 +0300

    Remove title change handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => setTitle("Labas");
-
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">{props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change title</button>
       </Card>
     </li>
   );
